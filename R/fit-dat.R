e1c2d7d93f8172788f26858c906c1241a61dcdc5
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2023-03-20 10:41:52 -0400

    add package data

---
+++ R/fit-dat.R
@@ -0,0 +1,8 @@
+#' @title Simulated Data for Testing Functions
+#'
+#' @description A matrix with randomly simulated data to test the package
+#' functions.
+#'
+#' @format A student-by-item matrix frame with 1000 rows (i.e., 1000 students)
+#' and 8 columns (i.e., 8 items).
+"fit_dat"
